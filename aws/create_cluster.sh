b8548ebebcd1e0a3eab8612d797da9a04067a6db
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2020-03-18 08:30:09 +0100

    added AWS create script

---
+++ aws/create_cluster.sh
@@ -0,0 +1,22 @@
+eksctl create cluster \
+--name xcube-gen \
+--region eu-central-1 \
+--nodegroup-name xcube-gen-workers \
+--node-type t3.medium \
+--nodes 3 \
+--nodes-min 1 \
+--nodes-max 4 \
+--managed
+
+
+eksctl utils associate-iam-oidc-provider --cluster=xcube-gen --approve
+
+aws iam create-policy     --policy-name ALBIngressControllerIAMPolicyXC     --policy-document https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json
+
+eksctl create iamserviceaccount --cluster=xcube-gen --namespace=kube-system --name=alb-ingress-controller --attach-policy-arn=arn:aws:iam::346516713328:policy/ALBIngressControllerIAMPolicy --override-existing-serviceaccounts --approve
+
+curl -sS "https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml" \
+     | sed "s/# - --cluster-name=devCluster/- --cluster-name=xcube-gen/g" \
+     | kubectl apply -f -
+
+# kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=xcube-gen:default
