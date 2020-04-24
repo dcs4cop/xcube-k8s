d2d793c8f6c62e7ec7f378fe6202f9a26d8df064
helge <lmn9p5zktm&Ptu>
2020-04-24 11:39:21 +0200

    Finalised demo version for xcube version 0.4.0.dev0

--- aws/README.md
+++ aws/README.md
@@ -2,7 +2,13 @@
 
 ## Installation
 
+Adapt xcube-gen-cluster.yaml and deploy-tls-termination.yaml to your needs.
+For the tls nginx ingress controller, pelase refer to its 
+[Installation Guide](https://kubernetes.github.io/ingress-nginx/deploy/)  
+
+
 ```bash
 eksctl create cluster -f xcube-gen-cluster.yaml
+kubectl apply -f deploy-tls-termination.yaml
 ```
 
--- aws/create_cluster.sh
+++ aws/create_cluster.sh
@@ -1,30 +1,3 @@
-eksctl create cluster \
---name xcube-gen \
---region eu-central-1 \
---nodegroup-name xcube-gen-workers \
---node-type t3.large \
---nodes 3 \
---nodes-min 1 \
---nodes-max 4 \
---managed
+eksctl create cluster -f xcube-gen-cluster.yaml
 
-
-eksctl utils associate-iam-oidc-provider --cluster=dcfs-xcube-gen --approve
-
-aws iam create-policy \
-  --policy-name ALBIngressControllerIAMPolicy \
-  --policy-document https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json
-
-eksctl create iamserviceaccount \
-  --cluster=xcube-gen \
-  --namespace=kube-system \
-  --name=alb-ingress-controller \
-  --attach-policy-arn=arn:aws:iam::346516713328:policy/ALBIngressControllerIAMPolicy \
-  --override-existing-serviceaccounts \
-  --approve
-
-kubectl apply -f alb-ingress-controller.yaml
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.2/docs/examples/rbac-role.yaml
-
-# kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=xcube-gen:default
+kubectl apply -f deploy-tls-termination.yaml
